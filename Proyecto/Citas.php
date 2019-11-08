62e43dd4961dbf6f39ebb9211a7f49286301fe30
Elihu <sala@HOR-AULA501-017.unihorizonte.salas>
2019-11-08 17:13:48 -0500

    Senamedico2

---
+++ Proyecto/Citas.php
@@ -0,0 +1,40 @@
+<?php
+?>
+<DOCTYPE html>
+<html>
+<head>
+    <title>Citas</title>
+	<meta charset="UTF-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body background="../imagenes/sena.jpg">
+<br/>
+<br/>
+<center> 
+<H2>Seleccione un tipo de cita</H2>
+<br/>
+    
+  <button class="btn btn-secondary" style="width: 500px;height:50px;">
+    Cita Médica
+  </button>
+        <br/>
+        <br/>
+  <button class="btn btn-secondary" style="width: 500px;height:50px;">
+    Cita Odontológica
+  </button> 
+        <br/>
+        <br/>
+  <button class="btn btn-secondary" style="width: 500px;height:50px;">
+    Cita Prioritaria
+  </button>
+        <br/>
+        <br/>
+  <button class="btn btn-secondary" style="width: 500px;height:50px;">
+    Urgencias
+  </button>
+  
+</center>
+</body>
+</html>
+
+
