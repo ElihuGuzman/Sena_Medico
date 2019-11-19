9d9b7f806e06ce74663792601e855090372e1273
Elihu <sala@HOR-AULA501-017.unihorizonte.salas>
2019-11-19 16:33:46 -0500

    Senamedico2

--- Proyecto/Citas.php
+++ Proyecto/Citas.php
@@ -11,29 +11,37 @@
 <br/>
 <br/>
 <center> 
-<H2>Seleccione un tipo de cita</H2>
+<H2>Seleccione un tipo de cita para programarla</H2>
 <br/>
-    
-  <button class="btn btn-secondary" style="width: 500px;height:50px;">
-    Cita Médica
-  </button>
+  
+<form>
+  <input class="btn btn-secondary" style="width: 500px;height:50px;" name="button2" type="button" 
+    accept="" onclick='alert("Su cita ha sido programada exitosamente Sede: Soacha Doctor(a): Alan Grant Fecha: 30/11/2019 Consultorio: #304 Precio: 3.000$")' value="Programar Cita Médica"/>
+  </form>
         <br/>
+        
+  <form>
+  <input class="btn btn-secondary" style="width: 500px;height:50px;" name="button2" type="button" 
+    accept="" onclick='alert("Su cita ha sido programada exitosamente Sede: Kennedy Doctor(a): Ellie Satler Fecha: 07/12/2019 Consultorio: #301 Precio: 5.000$")' value="Programar Cita Odontológica"/>
+  </form>
         <br/>
-  <button class="btn btn-secondary" style="width: 500px;height:50px;">
-    Cita Odontológica
-  </button> 
+  <form>
+  <input class="btn btn-secondary" style="width: 500px;height:50px;" name="button2" type="button" 
+    accept="" onclick='alert("Su cita ha sido programada exitosamente Sede: Suba Doctor(a): Lewis Dogson Fecha: 15/01/2020 Consultorio: #11 Precio: 15.000$")' value="Programar Cita Prioritaria"/>
+  </form>
         <br/>
-        <br/>
-  <button class="btn btn-secondary" style="width: 500px;height:50px;">
-    Cita Prioritaria
-  </button>
-        <br/>
-        <br/>
-  <button class="btn btn-secondary" style="width: 500px;height:50px;">
+        <a href="Urgencias.php"><button class="btn btn-secondary" style="width: 500px;height:50px;">
     Urgencias
-  </button>
+  </button></a>
   
 </center>
+<br/>
+<br/>
+<br/>
+<br/>
+<a href="index.php"><button class="btn btn-danger" style="width: 100px;height:50px;margin-left: 1200px;">
+    X Salir 
+  </button></a>
 </body>
 </html>
 
